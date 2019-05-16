9b53daa4db5ba53907fcfbd6dc54a1e7ae5c531c
littlexiaotimpro <a99996655@163.com>
2019-05-16 12:54:07 +0800

    修改首页样式...

--- src/client_pages/home/Home.jsx
+++ src/client_pages/home/Home.jsx
@@ -1,17 +1,16 @@
 import React, {Component} from "react";
 import "./less/home.css";
 
+const lots = ["上上签", "上吉签", "大吉签", "中吉签", "中平签", "中下签", "下吉签", "下平签", "下下签"];
+
 class Home extends Component {
     render() {
         return (
             <div>
                 <div className={"card-box"}>
-                    <p style={{marginTop: 80}}>加班成常态，学习到变态 ！！！</p>
+                    <p style={{marginTop: 200}}>加班成常态，学习到变态 ！！！</p>
                     <p>少年，你够变态了吗 ！！！</p>
                     <p>程序员不止眼前的逻辑和代码，还有底层的架构与实现。</p>
-                    <p>少年，你说你还缺对象吗 ！！！</p>
-                    <p>除了诗与远方，和眼前的苟且，还有永无止境的bug。</p>
-                    <p>少年，你还说你不会写bug ！！！</p>
                 </div>
             </div>
         )
